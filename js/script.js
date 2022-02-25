7f07fd4f8c82ec2efdf43442f9f0672cadc5d8f9
MattiaPortanti <mattia.portantipm@gmail.com>
2022-02-25 19:21:35 +0100

    sistemazioni grafiche e aggiunta classi

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ console.log(mailList);
 //TODO creato un bottone tramite javascript
 let newButton = document.createElement("button");
 //TODO aggiunte le classi
-newButton.classList.add("btn", "btn-primary");
+newButton.classList.add("btn", "btn-warning", "btn-lg");
 //TODO aggiunto il testo all'suo interno
 newButton.innerHTML = "Accedi"
 //TODO aggiunto il bottone nel HTML alla fine del main
@@ -20,8 +20,11 @@ document.querySelector("#command").append(newButton);
 //TODO aggiunta la classe text-center al parent del bottone
 newButton.parentElement.classList.add("text-center");
 
+const inputLabel = document.querySelector("label");
+inputLabel.classList.add("mb-3", "fst-italic");
+
 let newMessage = document.querySelector("p");
-newMessage.classList.add("fs-3", "fw-bold");
+newMessage.classList.add("fs-3", "fw-bold", "text-white");
 
 //! aggiungo un eventlistner al button per far si che dopo che l'utente inserisce la sua e-mail nell'input,
 //! e clicca il bottone accedi il ciclo FOR scorra la lista
@@ -29,7 +32,6 @@ newButton.addEventListener("click", function() {
     //! variabile che prende il valore del input con id = user-mail
     let userMail = document.getElementById("user-mail").value;
     // console.log(userMail);
-
     
     //! inizializzo un ciclo for che farà scorrere la lista di e-mail dall'inizio alla fine
     for (let i = 0; i < mailList.length; i = i + 1) {
@@ -45,6 +47,7 @@ newButton.addEventListener("click", function() {
     } else {
         newMessage.innerHTML = "Your credentials are invalid!"
     }
+
 }
 
 })
